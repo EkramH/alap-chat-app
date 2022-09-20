f0c7f28874aff5b037a022df2497211fedac2b50
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-20 10:37:30 +0600

    pages

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Register</h2>
+    <div className="form__container">
+      <div className="form__wrapper">
+        <span className="form__logo">Alap Chat</span>
+        <span className="form__title">Register</span>
+        <form>
+          <input type="text" name="name" id="Display Name" />
+          <input type="email" name="email" id="Your Email" />
+          <input type="password" name="password" id="Password" />
+          <input type="file" />
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
     </div>
   );
 };
