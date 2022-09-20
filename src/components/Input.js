b8bfe7034ea308c51ceeb7589eaa1972feb5ccdf
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-20 22:28:36 +0600

    message input design

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./components.css";
+import img from "../assets/img.png";
+import attach from "../assets/attach.png";
 
 const Input = () => {
   return (
     <div className="input">
-      <h4>input</h4>
+      <input
+        className="input__text"
+        type="text"
+        placeholder="Type something..."
+      />
+      <div className="input__send">
+        <img src={attach} alt="" />
+        <input type="file" id="file" style={{ display: "none" }} />
+        <label htmlFor="file">
+          <img src={img} alt="" />
+        </label>
+        <button className="input__btn">Send</button>
+      </div>
     </div>
   );
 };
