53450b9e70dd9e9edb5b3b81f65c4748fb8d9002
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-20 11:18:39 +0600

    login and register page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./pages.css";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="form__container">
+      <div className="form__wrapper">
+        <span className="form__logo">Alap Chat</span>
+        <span className="form__title">Register</span>
+        <form className="form__box">
+          <input type="email" name="email" placeholder="Your Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button className="form__btn">Sign up</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
     </div>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./pages.css";
+import add from "../assets/addAvatar.png";
 
 const Register = () => {
   return (
@@ -6,12 +8,16 @@ const Register = () => {
       <div className="form__wrapper">
         <span className="form__logo">Alap Chat</span>
         <span className="form__title">Register</span>
-        <form>
-          <input type="text" name="name" id="Display Name" />
-          <input type="email" name="email" id="Your Email" />
-          <input type="password" name="password" id="Password" />
-          <input type="file" />
-          <button>Sign up</button>
+        <form className="form__box">
+          <input type="text" name="name" placeholder="Display Name" />
+          <input type="email" name="email" placeholder="Your Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="file" id="file" style={{ display: "none" }} />
+          <label htmlFor="file">
+            <img src={add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button className="form__btn">Sign up</button>
         </form>
         <p>You do have an account? Login</p>
       </div>
