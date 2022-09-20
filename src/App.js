2d46039a35f2e61824a783272f006ff317ccf9af
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-20 13:13:05 +0600

    chat header and info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 
 function App() {
   return (
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./components.css";
+import cam from "../assets/cam.png";
+import add from "../assets/add.png";
+import more from "../assets/more.png";
 
 const Chat = () => {
   return (
     <div className="chat">
-      <h4>Chat</h4>
+      <div className="chat__info">
+        <span>Ekram</span>
+        <div className="chat__icon">
+          <img src={cam} alt="" />
+          <img src={add} alt="" />
+          <img src={more} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
