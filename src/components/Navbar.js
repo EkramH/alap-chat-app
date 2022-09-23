45ea75eadb03d38b1a8400dc7162c6d47e51cf4d
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-23 12:22:41 +0600

    user name and img

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
 import { signOut } from "firebase/auth";
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { auth } from "../firebase.init";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="navbar__logo">Alap Chat</span>
       <div className="navbar__user">
-        <img
-          className="navbar__img"
-          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-          alt=""
-        />
-        <span className="nabvar__user">Ekram</span>
+        <img className="navbar__img" src={currentUser.photoURL} alt="" />
+        <span className="nabvar__user">{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)} className="navbar__btn">
           Logout
         </button>
