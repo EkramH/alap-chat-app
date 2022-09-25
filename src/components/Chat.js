f3aaa6adc96839562ddf3c0be2d64066b7453f77
Ekram Hosen <ekramhosen329@gmail.com>
2022-09-25 21:20:58 +0600

    user data at chat navbar

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./components.css";
 import cam from "../assets/cam.png";
 import add from "../assets/add.png";
 import more from "../assets/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="chat">
       <div className="chat__info">
-        <span>Ekram</span>
+        <span>{data.user?.displayName}</span>
         <div className="chat__icon">
           <img src={cam} alt="" />
           <img src={add} alt="" />
